bb633ca7ab9047fbb6d7ab014abd9f66ae0b14b9
lexson-luo <lisheng.luo@outlook.com>
2023-10-12 17:52:01 +0800

    update realtime_vwap_vol.py

--- realtime_vwap_vol.py
+++ realtime_vwap_vol.py
@@ -1,8 +1,9 @@
-import websockets
 import logging
 import asyncio
-import time
+import websockets
 import json
+import time
+
 import pandas as pd
 import numpy as np
 
@@ -18,20 +19,18 @@ timeseries = {"timestamp": [], "vwap": []}
 n_trades = 0
 sum_price_size = 0
 sum_size = 0
-close = 0
 
 
 async def subscribe():
     async with websockets.connect(URL) as ws:
+        global n_trades, sum_price_size, sum_size
         while ws.open:
             message = await ws.recv()
             trade_data = json.loads(message)
-            global n_trades, sum_price_size, sum_size, close
-            _trade_price = float(trade_data["p"])
-            _trade_size = float(trade_data["q"])
-            sum_price_size += _trade_price * _trade_size
-            sum_size += _trade_size
-            close = _trade_price
+            _traded_price = float(trade_data["p"])
+            _traded_size = float(trade_data["q"])
+            sum_price_size += _traded_price * _traded_size
+            sum_size += _traded_size
             n_trades += 1
 
 
@@ -40,30 +39,23 @@ async def compute_vwap_vol(interval_secs, window_size):
 
     while True:
         await asyncio.sleep(interval_secs)
-
-        logging.info("Compute VWAP volatility")
-        if (
-            sum_price_size == 0
-        ):  # skip-div-error/continue, in case no trading (ie. 0 volume) or 0 price.
+        if sum_price_size == 0:
             continue
 
         dt = pd.to_datetime(time.time(), unit="s")
-        vwap = sum_price_size / sum_size
-
         timeseries["timestamp"].append(dt)
-        timeseries["vwap"].append(vwap)
+        timeseries["vwap"].append((sum_price_size / sum_size))
 
         df = pd.DataFrame(timeseries)
         df.set_index("timestamp", inplace=True)
         df["log_returns"] = np.log(df["vwap"] / df["vwap"].shift())
-        df["vol"] = df["log_returns"].std()
-
+        vwap_vol = df["log_returns"].std()
         vol_scale = np.sqrt(24 * 60 * 60 / interval_secs)
-        vol_daily = df["vol"][-1] * vol_scale
+        vwap_vol_scaled = vol_scale * vwap_vol
 
         logging.info(
-            f"5-min VWAP vol: scaled-to-daily-vwap-vol= {vol_daily:.4f}, vwap={vwap:.4f}, n={n_trades}, volume={sum_size:.3f}"
-        )  # 5-min because 5secs * 60 (window_size)
+            f"VWAP Vol: scaled_to_daily= {vwap_vol_scaled:.4f}, n_trades= {n_trades}, vwap={df['vwap'][-1]:.4f}, volume= {sum_size:.3f}, window_size= {df.shape[0]}"
+        )
 
         n_trades = 0
         sum_price_size = 0
@@ -75,8 +67,7 @@ async def compute_vwap_vol(interval_secs, window_size):
 
 
 async def start():
-    logging.info("Starting...")
-
+    logging.info("Start computing 5min vwap vol...")
     await asyncio.gather(subscribe(), compute_vwap_vol(5, 60))
 
 
